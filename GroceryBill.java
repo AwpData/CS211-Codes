cb7160d406ae0a29707759d80e450da797045055
AwpData <awpdata@gmail.com>
2019-09-20 15:11:51 -0700

    Update GroceryBill.java

--- GroceryBill.java
+++ GroceryBill.java
@@ -2,20 +2,18 @@ import java.util.ArrayList;
 
 public class GroceryBill {
 	private ArrayList<Item> bill;
-	private int total;
 	private boolean preferred;
 
-	public GroceryBill(String clerk, boolean preferred) {
-		this.total = 0;
+	public GroceryBill(Employee Clerk, boolean preferred) { // I added preferred to use in the printReceipt() method if the person has discounts
 		this.bill = new ArrayList<>();
 		this.preferred = preferred;
 	}
 
-	public void add(Item i) {
+	public void add(Item i) { // adds the item to the bill
 		bill.add(i);
 	}
 
-	public double getTotal() {
+	public double getTotal() { // The total is calculated without any discounts
 		double total = 0;
 		for (Item item : bill) {
 			total += item.getPrice();
@@ -23,16 +21,20 @@ public class GroceryBill {
 		return total;
 	}
 
-	public ArrayList<Item> getBill() {
+	public ArrayList<Item> getBill() { // Getter method for the ArrayList of items in bill
 		return this.bill;
 	}
 
-	public void printReceipt() {
-		for (Item item : bill) {
-			if (preferred) {
-				System.out.println(item.getItemName() + " " + item.getPrice() + " - discount " + item.getDiscount() + " = " + (item.getPrice() - item.getDiscount()));
-			} else {
-				System.out.println(item.getItemName() + " " + item.getPrice());
+	public void printReceipt() { // Prints each item on receipt (totals are calculated in client)
+		if (bill.size() == 0) {
+			System.out.println("Nothing purchased!");
+		} else {
+			for (Item item : bill) {
+				if (preferred) {
+					System.out.println(item.getItemName() + " $" + (item.getPrice() - item.getDiscount()) + " (discount of $" + item.getDiscount() + ") off of $" + item.getPrice());
+				} else {
+					System.out.println(item.getItemName() + " $" + item.getPrice());
+				}
 			}
 		}
 	}
