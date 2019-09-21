b9e3b572fc7c2b725f7a7dec62f84f74adf31599
AwpData <awpdata@gmail.com>
2019-09-20 20:33:23 -0700

    Update GroceryBill.java

--- GroceryBill.java
+++ GroceryBill.java
@@ -4,6 +4,11 @@ public class GroceryBill {
 	private ArrayList<Item> bill;
 	private boolean preferred;
 
+	public GroceryBill() { // Default Constructor
+		this.bill = new ArrayList<>();
+		this.preferred = false;
+	}
+
 	public GroceryBill(Employee Clerk, boolean preferred) { // I added preferred to use in the printReceipt() method if the person has discounts
 		this.bill = new ArrayList<>();
 		this.preferred = preferred;
@@ -11,6 +16,22 @@ public class GroceryBill {
 
 	public void add(Item i) { // adds the item to the bill
 		bill.add(i);
+	}
+
+	public void printReceipt() { // Prints each item on receipt (totals are calculated in client)
+		int counter = 1; // Used to list items in receipt
+		if (bill.size() == 0) {
+			System.out.println("Nothing purchased!\n");
+		} else {
+			for (Item item : bill) {
+				if (preferred) {
+					System.out.printf("%d. %s $%.2f (discount of $%.2f) off of $%.2f\n", counter, item.getItemName(), (item.getPrice() - item.getDiscount()), item.getDiscount(), item.getPrice());
+				} else {
+					System.out.printf("%d. %s $%.2f", counter, item.getItemName(), item.getPrice());
+				}
+				counter++;
+			}
+		}
 	}
 
 	public double getTotal() { // The total is calculated without any discounts
@@ -23,19 +44,5 @@ public class GroceryBill {
 
 	public ArrayList<Item> getBill() { // Getter method for the ArrayList of items in bill
 		return this.bill;
-	}
-
-	public void printReceipt() { // Prints each item on receipt (totals are calculated in client)
-		if (bill.size() == 0) {
-			System.out.println("Nothing purchased!");
-		} else {
-			for (Item item : bill) {
-				if (preferred) {
-					System.out.println(item.getItemName() + " $" + (item.getPrice() - item.getDiscount()) + " (discount of $" + item.getDiscount() + ") off of $" + item.getPrice());
-				} else {
-					System.out.println(item.getItemName() + " $" + item.getPrice());
-				}
-			}
-		}
 	}
 }
