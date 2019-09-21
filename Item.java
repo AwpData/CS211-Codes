474fd1b4688412ac4a4c28438f2e52db8bb55193
AwpData <awpdata@gmail.com>
2019-09-20 21:08:45 -0700

    Update Item.java

--- Item.java
+++ Item.java
@@ -2,11 +2,13 @@ public class Item {
 	private String itemname;
 	private double price;
 	private double discount;
+	private double itemDiscountPercent; // Added for printReceipt() method in GroceryBill to print the percentage discount
 
-	public Item(String itemname, double price, double discount) { // Constructs new item with name, price, and discount
+	public Item(String itemname, double price, double discount, double itemDiscountPercent) { // Constructs new item with name, price, and discount
 		this.itemname = itemname;
 		this.price = price;
 		this.discount = discount;
+		this.itemDiscountPercent = itemDiscountPercent;
 	}
 
 	public String getItemName() { // Gets the name of this item
@@ -19,5 +21,9 @@ public class Item {
 
 	public double getDiscount() { // Gets the discount of this item
 		return this.discount;
+	}
+
+	public double getitemDiscountPercent() {
+		return this.itemDiscountPercent;
 	}
 }
