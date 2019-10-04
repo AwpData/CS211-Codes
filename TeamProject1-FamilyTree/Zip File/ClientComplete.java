c4be5bf87f288f68ae91446f5167c06aa6802227
AwpData <awpdata@gmail.com>
2019-10-04 15:59:38 -0700

    Update ClientComplete.java

--- TeamProject1-FamilyTree/Zip File/ClientComplete.java
+++ TeamProject1-FamilyTree/Zip File/ClientComplete.java
@@ -11,7 +11,7 @@ import java.util.*;
 public class ClientComplete {
 	public static void main(String[] args) throws FileNotFoundException {
 		Scanner input = new Scanner(System.in);
-		Scanner fileReader = new Scanner(new File("src/people.txt"));
+		Scanner fileReader = new Scanner(new File("people.txt"));
 		boolean programRunning = true;
 		ArrayList<String> names = new ArrayList<String>();
 		ArrayList<Person> familyStructures = new ArrayList<Person>();
