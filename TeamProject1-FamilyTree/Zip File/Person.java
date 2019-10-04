8472047b647c712d324ff5c8f3742ec3bb0099a3
AwpData <awpdata@gmail.com>
2019-10-04 14:14:43 -0700

    Update Person.java

--- TeamProject1-FamilyTree/Zip File/Person.java
+++ TeamProject1-FamilyTree/Zip File/Person.java
@@ -1,22 +1,27 @@
+/* Names: Trevor Tang, Sudhanva Donakonda, Anas Gherfal, Hacer Aynur Sari
+ * Course: CS 211
+ * Date: October 4, 2019
+ * Reason: To store a person's name, their mother, and their father with getter methods 
+ */
 
 public class Person {
 	private String mother, father, person;
-	
+
 	public Person(String person, String mother, String father) {
 		this.person = person;
 		this.mother = mother;
 		this.father = father;
 	}
-	
+
 	public String getPerson() {
 		return this.person;
 	}
-	
+
 	public String getMother() {
 		return this.mother;
 	}
-	
+
 	public String getFather() {
 		return this.father;
-	}	
+	}
 }
