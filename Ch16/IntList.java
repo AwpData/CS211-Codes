b7dd063f77feb0052a09c071ca37635e7bef115e
AwpData <awpdata@gmail.com>
2019-10-27 12:07:36 -0700

    Update IntList.java

--- Ch16/IntList.java
+++ Ch16/IntList.java
@@ -1,13 +1,25 @@
-/*
- This is the interface definition for the IntList data type
+/* Name: Trevor Tang
+ * Course: CS 211
+ * Date: October 27, 2019
+ * Reason: Given interface that was implemented in LinkedIntList.java
  */
- 
+
+/*
+ * This is the interface definition for the IntList data type
+ */
+
 public interface IntList {
-    public int size();
-    public int get(int index);
-    public String toString();
-    public int indexOf(int value);
-    public void add(int value);
-    public void add(int index, int value);
-    public void remove(int index);
+	public int size();
+
+	public int get(int index);
+
+	public String toString();
+
+	public int indexOf(int value);
+
+	public void add(int value);
+
+	public void add(int index, int value);
+
+	public void remove(int index);
 }
